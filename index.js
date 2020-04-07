aff6793aa3ff8134c92ed83c02b0eb03fe68a024
arkyetc <63299666+arkyetc@users.noreply.github.com>
2020-04-07 14:39:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let name
+let age
+let jobRole = "European Service Desk Manager"
+
+
+function personalData(name, age) {
+    console.log("Hello, I am " + name + " and I am " + age + " years old.")
+}
+
+personalData("Lewis", 27)
+personalData("Tommy", 27)
+personalData("Ollie", 42)
+personalData("Chris", 12)
+let numberOne
+let numberTwo
+
+function addition(numberOne, numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+}
+
+addition(50, 40)
+addition(30, 50)
